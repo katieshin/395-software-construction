e36e9d8e434bb71421b9c9b1a0bbdd3314000364
skorlir <skorlir@gmail.com>
2017-04-03 23:40:18 -0500

    Update ITurn to reflect changes to Turn spec.

--- ITurn.java
+++ ITurn.java
@@ -1,10 +1,13 @@
 interface ITurn {
-    // player rolls the die for this turn, record the result with state 
+    // player rolls the die for this turn, record the result with state
     int /* 1 .. 6 */ roll();
 
-	// whether the turn was skipped
-	boolean skipped = false;
+    // player chooses to hold their current score for the remainder of the game
+    void hold();
 
-    // player skips this turn 
-    void skip(); 
+    // player chooses to roll the die twice and sum the results
+    int rollTwice();
+
+    // player skips this turn
+    void skip();
 }
